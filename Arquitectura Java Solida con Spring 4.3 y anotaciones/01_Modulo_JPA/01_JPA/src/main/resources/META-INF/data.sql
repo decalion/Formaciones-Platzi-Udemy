5b816c4b858fb52b6019a361efe5ce79b943f731
Ismael Caballero <icaballerohernandez@gmail.com>
2019-10-27 02:51:11 +0200

    Arquitectura Java , JPA

---
+++ Arquitectura Java Solida con Spring 4.3 y anotaciones/01_Modulo_JPA/01_JPA/src/main/resources/META-INF/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Noticia (titulo,autor,fecha) VALUES ('JAVA 11 aha salido','Ismael','2019-10-26');
+INSERT INTO Comentario(texto,autor,noticia_titulo) VALUES ('bien!!!','juan','JAVA 11 aha salido');
