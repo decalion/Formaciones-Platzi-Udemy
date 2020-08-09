385df27ee992dfe248fc1257b19c899bb69ae91c
Ismael Caballero <icaballerohernandez@gmail.com>
2020-08-09 22:07:40 +0200

    Ficheros yml, despliegue war tomcat, actuator y certificado

---
+++ Spring boot/06_SpringBootWar/src/main/java/com/icaballero/app/SpringBootYmlApplication.java
@@ -0,0 +1,11 @@
+package com.icaballero.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootYmlApplication  extends SpringBootServletInitializer{
+
+	
+
+}
